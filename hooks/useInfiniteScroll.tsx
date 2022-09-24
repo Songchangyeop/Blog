5f182c85913ac7182098854474abe16ae180be87
Songchangyeop <waitwait0301@naver.com>
2022-09-25 04:56:22 +0900

    feat: useInfiniteSctoll hook 생성

---
+++ hooks/useInfiniteScroll.tsx
@@ -0,0 +1,34 @@
+import React, { useCallback, useEffect, useState } from 'react';
+
+interface PropTypes {
+  list?: {
+    date: string;
+    title: string;
+    des: string;
+    thumbnail: string;
+    tags: string[];
+    slug: string;
+  }[];
+}
+
+const useInfiniteScroll = (list): PropTypes => {
+  const [currentList, setCurrentList] = useState([]);
+
+  useEffect(() => {
+    const sliceList = list?.slice(0, 2);
+    setCurrentList(sliceList);
+  }, [list]);
+
+  const load = useCallback(() => {
+    const sliceList = list?.slice(currentList.length, currentList.length + 2);
+
+    setCurrentList((currentList) => currentList.concat(sliceList));
+  }, [currentList]);
+
+  return {
+    currentList,
+    load,
+  };
+};
+
+export default useInfiniteScroll;
