a453e0cad57649cafdd501d527afcbcc8915ea13
Songchangyeop <waitwait0301@naver.com>
2022-09-27 22:50:49 +0900

    docs: JsonServer 포스트 이동

--- .contentlayer/generated/Post/_index.mjs
+++ .contentlayer/generated/Post/_index.mjs
@@ -5,6 +5,7 @@ import jsAsyncMdx from './JS-Async.mdx.json'
 import jsCorsMdx from './JS-Cors.mdx.json'
 import jsGeolocationMdx from './JS-Geolocation.mdx.json'
 import jsHoistingMdx from './JS-Hoisting.mdx.json'
+import jsJsonServerMdx from './JS-JsonServer.mdx.json'
 import nextAppDocMdx from './Next-AppDoc.mdx.json'
 import nextImageMdx from './Next-Image.mdx.json'
 import nextPrerenderingMdx from './Next-Prerendering.mdx.json'
@@ -12,4 +13,4 @@ import reactAntd_paginationMdx from './React-Antd_Pagination.mdx.json'
 import reactAxiosInstanceMdx from './React-AxiosInstance.mdx.json'
 import reactKakaoMapMdx from './React-KakaoMap.mdx.json'
 
-export const allPosts = [couchRetrospectMdx, jsAsyncMdx, jsCorsMdx, jsGeolocationMdx, jsHoistingMdx, nextAppDocMdx, nextImageMdx, nextPrerenderingMdx, reactAntd_paginationMdx, reactAxiosInstanceMdx, reactKakaoMapMdx]
+export const allPosts = [couchRetrospectMdx, jsAsyncMdx, jsCorsMdx, jsGeolocationMdx, jsHoistingMdx, jsJsonServerMdx, nextAppDocMdx, nextImageMdx, nextPrerenderingMdx, reactAntd_paginationMdx, reactAxiosInstanceMdx, reactKakaoMapMdx]
