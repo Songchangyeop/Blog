003c984b8bfe3fac37ed948c006b59b41730eeb8
Changyeop Song <waitwait0301@naver.com>
2024-02-26 23:15:30 +0900

    feat: openGraph 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,25 @@ import { RecoilRoot } from 'recoil';
 import '../styles/globals.css';
 
 import Layout from 'components/Layout';
+import { NextSeo } from 'next-seo';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
+      <NextSeo
+        openGraph={{
+          type: 'website',
+          locale: 'ko_KR',
+          url: 'https://song-dev.vercel.app/',
+          title: 'Dev.Song | Blog',
+          images: [
+            {
+              url: '/public/Profile.jpg',
+              alt: 'og:image',
+            },
+          ],
+        }}
+      />
       <Layout>
         <Component {...pageProps} />
       </Layout>
