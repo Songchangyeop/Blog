7de504f606d76e93745938abe29ef1f457000ba3
Changyeop Song <waitwait0301@naver.com>
2024-03-18 20:13:00 +0900

    feat: 기능 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,29 +24,7 @@ const Home = ({ posts }: PropTypes) => {
         <Profile>
           <Image src={ProfileImage.src} alt="profile" />
         </Profile>
-        <Intro>
-          소통과 공유를 바탕으로 기본을 중요시하는 프론트엔드 개발자가 되고자
-          합니다.
-          <br />
-          새로운 기술을 익힐 때 프로젝트에 대한 기획을 하고 새로 익힌 기술을
-          통해 프로젝트를 진행하는 것에 재미를 느낍니다.
-          <br />
-          그저 배움에서 그치지 않고 능동적으로 프로젝트에 적용하고, 개선점을
-          찾기 위해 노력합니다.
-          <br />
-          <br />
-          song.dev
-          <br />
-          Front-end Engineer / Seoul, Korea
-          <br />
-          <br />
-          {/* <ResumeLink
-            href="https://holly-monarch-69a.notion.site/e2c53fb57c8c4730a5cd10779137d0c6"
-            target="_blank"
-          >
-            Resume (Notion)
-          </ResumeLink> */}
-        </Intro>
+        <Intro></Intro>
       </About>
       <RecentPosts posts={posts} />
     </Container>
